abcede12135dcac4de19dce63def93891ea9e054
likith P <likith.prasadam@gmail.com>
2026-06-04 13:28:01 +0530

    health check test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Application Healthy Version 2"
+    return "Application Healthy Version 3"
 
 
 if __name__ == "__main__":
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,4 +7,4 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.data.decode() == "Application Healthy Version 2"
+    assert response.data.decode() == "Application Healthy Version 3"
