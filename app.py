a3ec30027a6a870ad83e0c74724fdc66674d3eae
likith P <likith.prasadam@gmail.com>
2026-06-04 12:29:14 +0530

    Fix flake8 warnings

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template
-
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Application Healthy"
 
 
 if __name__ == "__main__":
