7710ab8131be743c94c51abf878a3635c42043cb
likith P <likith.prasadam@gmail.com>
2026-06-04 13:51:53 +0530

    rollback 2

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "BROKEN"
+    return "broken"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
